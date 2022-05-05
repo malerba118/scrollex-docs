4f25c0a57efa459632ee0d4d1b5bc9ddd4c833f9
Austin Malerba <austin.malerba@gmail.com>
2022-05-05 07:26:35 -0600

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 
@@ -15,6 +16,32 @@ if (typeof window !== "undefined") {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <NextSeo
+        title="Scrollex"
+        description="A react library to build beautiful scroll experiences with minimal code."
+        canonical="https://scrollex-docs.vercel.app/"
+        openGraph={{
+          url: "https://scrollex-docs.vercel.app/",
+          title: "Scrollex",
+          description:
+            "A react library to build beautiful scroll experiences with minimal code.",
+          images: [
+            {
+              url: "https://scrollex-docs.vercel.app/scrollex.png",
+              width: 734,
+              height: 490,
+              alt: "Scrollex",
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "Scrollex",
+        }}
+        twitter={{
+          handle: "@austin_malerba",
+          site: "@austin_malerba",
+          cardType: "summary_large_image",
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
